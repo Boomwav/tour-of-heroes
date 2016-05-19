ff725202bdaba6ba21dad084e0b3299e286f6d69
Simon Dufour <sim.dufour@gmail.com>
2016-05-18 22:46:56 -0400

    Continued the service torial. It's pretty basic. I'll continue later.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,34 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import {HeroDetailComponent} from './components/hero-detail.component';
 
 import { Hero } from './models/hero';
 
+import {HeroService} from './services/hero.service';
+
 @Component({
   selector: 'my-app',
   templateUrl: "app/app.component.html",
   styleUrls: ["css/app.css"],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
+  
+  constructor(private heroService: HeroService) {}
+  
   selectedHero: Hero;
-  public heroes = HEROES;
+  public heroes: Hero[];
   title = "Tour of Heroes";
+  
+  ngOnInit() {
+    this.getHeroes();
+  }
+  
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
   
   onSelect(hero: Hero) {
     this.selectedHero = hero; 
   }
 }
 
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
