b00859ee6623b3a6aa1f2f13272a9aa9f698de5d
Simon Dufour <sim.dufour@gmail.com>
2016-05-19 22:24:30 -0400

    Added slow hero service for testing.

--- app/services/hero.service.ts
+++ app/services/hero.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from "@angular/core";
 
+import { Hero } from '../models/hero';
 import {HEROES} from './hero.mock'
 
 @Injectable()
 export class HeroService {
     getHeroes() {
         return Promise.resolve(HEROES);
+    }
+    
+    // See the "Take it slow" appendix
+    getHeroesSlowly() {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+    );
     }
 }
